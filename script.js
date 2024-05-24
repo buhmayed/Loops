88b303960c284bcc05f97224e65a64302b8eca99
buhmayed <160067232+buhmayed@users.noreply.github.com>
2024-05-24 13:28:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// let i = 1; //control variable
+// while (i <= 10) { // conditional
+//   console.log("lets go Oilers!");
+//   console.log("BOMP BOMP.. BOMP BOMP BOMP");
+//   i++; // iterator
+// }
+
+// let i = 1;
+// while (i <= 25) {
+//   console.log("Hello world!");
+//   console.log(i);
+//   i++;
+// }
+
+// let num = 0;
+// while (num < 100) {
+//   console.log(num);
+//   num++;
+// }
+// console.log("Loop Finished");
+
+// var x = 101;
+// while (x <= 129) {
+//   if (x % 2 == 1) {
+//     console.log(x);
+//   }
+//   x++;
+// }
+
+// ADD all numbers from 1 to 10
+// ?? 1 + 2 + 3 + ... + 10 = ____
+
+// let i = 1;
+// let total = 0;
+// while (i <= 10) {
+//   total = total + i;
+//   i++;
+//   console.log(`i: ${i}`);
+//   console.log(`total: ${total}`);
+// }
+
+//ex1
+
+// let i = 1;
+// while (i <= 800) {
+//   console.log("HELLO");
+//   i++;
+// }
+
+// let z = 20;
+// while (z <= 500) {
+//   if (z % 5 == 0) {
+//     console.log(z);
+//   }
+//   z++;
+// }
+
+// do-while -10 to + 10
+// let y = -10
+
+// do {
+//   console.log(y)
+//   y++
+//   while (y <= )
+// }
+
+// do {
+//   var response = prompt("Type STOP to end");
+// } while (response != "STOP");
+
+// break statements & while(true) Loops
+while (true) {
+  let num = +prompt("Enter a number from 1 to 10");
+
+  if (num >= 1 && num <= 10) {
+    break;
+  }
+  alert("you werent listning");
+}
+
+let i = 25;
+while (true) {
+  console.log(i);
+  i--;
+  if ((i = 1)) {
+    break;
+  }
+}
